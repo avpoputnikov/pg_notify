46de0f8eb97f381f2745ae2fd9a6b916879581d0
Andrey Poputnikov <avpoputnikov@yauzasystems.ru>
2019-12-18 17:16:15 +0300

    191218T1714

--- App.java
+++ App.java
@@ -10,7 +10,6 @@ public class App
         Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/tdb?currentSchema=public";
         Connection conn = DriverManager.getConnection(url,"postgres","postgres");
-        Listener listener = new Listener(conn);
-        listener.start();
+        new Listener(conn).start();
     }
 }
